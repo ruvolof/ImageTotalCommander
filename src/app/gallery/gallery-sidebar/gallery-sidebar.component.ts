8259920c31855875abb8e98991de7485a11bdeae
Francesco Ruvolo <ruvolof@gmail.com>
2020-12-22 23:06:23 +0100

    Adding sidebar button to close the slider.

--- src/app/gallery/gallery-sidebar/gallery-sidebar.component.ts
+++ src/app/gallery/gallery-sidebar/gallery-sidebar.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 interface WebkitFileInterface extends File {
   webkitRelativePath: string;
@@ -13,17 +13,25 @@ interface WebkitFileInterface extends File {
 export class GallerySidebarComponent implements OnInit {
   // Emits a File[] with images in the selected folder.
   @Output() imagesInFolder = new EventEmitter();
+  // Emits when the button to close the slider is clicked.
+  @Output() closeSlider = new EventEmitter();
+
+  @Input() isSliderVisible!: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public onCloseSliderClick(): void {
+    this.closeSlider.emit();
+  }
+
   public onDirectorySelected(fileList: FileList): void {
     const imagesInSelectedFolder = Array.from(fileList).filter(
       (file: WebkitFileInterface) => {
         if (file.type.startsWith('image/') 
-            && file.webkitRelativePath.split('/').length == 2) {
+            && file.webkitRelativePath.split('/').length === 2) {
           return file;
         }
         return;
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -8,17 +8,29 @@ import {Component, OnInit} from '@angular/core';
 export class GalleryComponent implements OnInit {
   public selectedImageIndex = -1;
   public imagesArray: File[] = [];
+  public isSliderVisible = false;
+  public isGridVisible = true;
 
   constructor() { }
 
   ngOnInit(): void { }
 
+  public onCloseSlider(): void {
+    this.selectedImageIndex = -1;
+    this.updateMainView();
+  }
+
   public onImagesInFolder(images: File[]): void {
-    console.log(images);
     this.imagesArray = images;
   }
 
   public onPictureSelected(index: number): void {
     this.selectedImageIndex = index;
+    this.updateMainView();
+  }
+
+  updateMainView(): void {
+    this.isSliderVisible = this.selectedImageIndex !== -1;
+    this.isGridVisible = !this.isSliderVisible;
   }
 }
